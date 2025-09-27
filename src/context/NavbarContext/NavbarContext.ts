759a1fd63372e4570064075b8255b1dd62b831f9
Lucas-Christian <lucas.christian.programmer@gmail.com>
2025-09-27 19:31:26 -0300

    refactor: removing navbar

--- src/context/NavbarContext/NavbarContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export type NavbarContextType = { mainId: number; handleNavigate: (index: number) => void; };
-
-
-export const NavbarContext = createContext<NavbarContextType>({
-  mainId: 0,
-  handleNavigate: () => undefined
-});
